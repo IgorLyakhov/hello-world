c486fd761d9528e7fb8281e04509bdab3f09325a
Igor Lyakhov <igorjan.97@mail.ru>
2020-03-25 02:22:47 +0400

    hello world program.

---
+++ src/curs/java/video/lesson1/MainLer.java
@@ -0,0 +1,13 @@
+package curs.java.video.lesson1;
+
+public class MainLer {
+    public static void main(String... args)  {
+        int i = 1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println(i);
+        i= i+1;
+        System.out.println(i);
+        System.out.println("Hello world!");
+    }
+}
